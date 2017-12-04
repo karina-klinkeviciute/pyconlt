b9282e0d57b17d395b454b050b57576b07e32017
Mantas <sirexas@gmail.com>
2017-12-04 15:33:05 +0200

    Small deployment related fixes
    - Renamed pycon_database.sql -> pycon_database.json, because django
      complains abount unknown "sql" fixture format.
    
    - Added setup.py in order to make it available in the virtualenv.
    
    - Added missing requirments.
    
    - Cheanged STATIC_ROOT location.

--- pyconlt/settings/base.py
+++ pyconlt/settings/base.py
@@ -82,13 +82,13 @@ WSGI_APPLICATION = 'pyconlt.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-    # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'pyconlt',  # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': 'pyconlt',
         'PASSWORD': 'password',
         'HOST': 'localhost',
-    # Empty for localhost through domain sockets or           '127.0.0.1' for localhost through TCP.
+        # Empty for localhost through domain sockets or           '127.0.0.1' for localhost through TCP.
         'PORT': '',  # Set to empty string for default.
     }
 }
@@ -132,7 +132,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'var', 'www', 'static')
 
 MEDIA_URL = '/media/'
 
