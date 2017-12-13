7b017c4a672510c424b036ca9a615e55ed3690af
Karina Klinkeviciute <karina.klinkeviciute@gmail.com>
2017-12-13 21:46:35 +0000

    debug false

--- pyconlt/settings/heroku.py
+++ pyconlt/settings/heroku.py
@@ -32,4 +32,4 @@ django_heroku.settings(locals())
 # import dj_database_url
 # db_from_env = dj_database_url.config(conn_max_age=500)
 # DATABASES['default'].update(db_from_env)
-DEBUG = True
+DEBUG = False
