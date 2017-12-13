f3d55f2c9fcdeff25289284b2c3c8f6e4cf0bd65
Karina Klinkeviciute <karina.klinkeviciute@gmail.com>
2017-12-13 20:55:25 +0000

    gunicorn added to reqs

--- pyconlt/settings/heroku.py
+++ pyconlt/settings/heroku.py
@@ -28,3 +28,7 @@ DEBUG = False
 # Configure Django App for Heroku.
 import django_heroku
 django_heroku.settings(locals())
+
+# import dj_database_url
+# db_from_env = dj_database_url.config(conn_max_age=500)
+# DATABASES['default'].update(db_from_env)
