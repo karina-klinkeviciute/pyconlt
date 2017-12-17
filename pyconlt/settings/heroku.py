b522bc6127fa4523f782812a335aa8df26e3450d
karina-klinkeviciute <karina.klinkeviciute@gmail.com>
2017-12-17 22:23:35 +0000

    heroku debug false

--- pyconlt/settings/heroku.py
+++ pyconlt/settings/heroku.py
@@ -32,7 +32,7 @@ django_heroku.settings(locals())
 # import dj_database_url
 # db_from_env = dj_database_url.config(conn_max_age=500)
 # DATABASES['default'].update(db_from_env)
-DEBUG = True
+DEBUG = False
 
 # MEDIA_URL = '/media/'
 #
