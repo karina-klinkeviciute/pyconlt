db792509bbf6b93d8ced9dfa7226a746569816db
karina-klinkeviciute <karina.klinkeviciute@gmail.com>
2018-01-07 21:22:28 +0000

    ordered queryset

--- presenters/views/presenter.py
+++ presenters/views/presenter.py
@@ -19,3 +19,10 @@ class PresentersView(ListView):
     template_name = 'presenters/presenters_list.html'
     context_object_name = 'presenters'
 
+    def get_queryset(self):
+        """
+        Order queryset by id. This way presenters that were entered first,
+        will stay on top. Just a temporary measure.
+        :return: ordered queryset
+        """
+        return super().get_queryset().order_by('id')
