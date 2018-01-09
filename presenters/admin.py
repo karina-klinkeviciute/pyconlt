04752258f0c5758b59e6c5b0a6133263c40a5f0f
Sarunas Navickas <saras@laptopas.namai>
2018-01-09 19:00:00 +0200

    Add groups to admin panel

--- presenters/admin.py
+++ presenters/admin.py
@@ -4,6 +4,17 @@ from presenters.models.presenter import Presenter
 
 class PresenterAdmin(admin.ModelAdmin):
     list_display = ('name', )
+    fieldsets = (
+        (None, {
+            'fields': ('user',), 
+        }),
+        ('Personal Info', {
+            'fields': ('name', 'image', 'bio', 'expertise')
+        }),
+        ('Social links', {
+            'fields': ('linkedin_handle', 'twitter_handle', 'github_handle')
+        })
+    )
 
 
 admin.site.register(Presenter, PresenterAdmin)
