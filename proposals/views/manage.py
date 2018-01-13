cbf8c7af7f76a062c3bc0acd52a24054267dfcee
Šarūnas Navickas <zaibacu@gmail.com>
2018-01-13 21:15:26 +0200

    Do some security

--- proposals/views/manage.py
+++ proposals/views/manage.py
@@ -1,8 +1,10 @@
 from django.views.generic import ListView
 from django.views.generic.edit import DeleteView, UpdateView
 from django.urls import reverse_lazy
+from django.utils.decorators import method_decorator
 
 from ..models.proposal import Proposal
+from ..security import OwnerRequiredMixin
 
 
 class ProposalListView(ListView):
@@ -14,12 +16,12 @@ class ProposalListView(ListView):
         return super().get_queryset().filter(user=self.request.user)
 
 
-class ProposalDeleteView(DeleteView):
+class ProposalDeleteView(DeleteView, OwnerRequiredMixin):
     model = Proposal
     success_url = reverse_lazy('proposal_list')
 
 
-class ProposalUpdateView(UpdateView):
+class ProposalUpdateView(OwnerRequiredMixin, UpdateView):
     model = Proposal
     fields = ('title', 'type', 'short_description', 'extra_info')
     template_name_suffix = '_update'
