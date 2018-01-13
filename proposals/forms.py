aefce4328a3fdf8ba63f9f548fe6d15c5e3ae414
Šarūnas Navickas <zaibacu@gmail.com>
2018-01-13 18:57:33 +0200

    Build form

--- proposals/forms.py
+++ proposals/forms.py
@@ -4,15 +4,33 @@ from django.utils.translation import ugettext_lazy as _
 from .models.proposal import Proposal, PROPOSAL_TYPE
 
 
-class CFPForm(forms.Form):
+class CFPForm(forms.ModelForm):
     type = forms.ChoiceField(
         label=_('Type of proposal'),
         choices=PROPOSAL_TYPE,
-        widget=forms.RadioSelect
+        required=True,
     )
-    # duration = forms.IntegerField()
+
+    short_description = forms.CharField(
+        label=_('Short description'),
+        required=True,
+    )
+
+    extra_info = forms.CharField(
+        label=_('Extra info'),
+        required=False,
+    )
+
+    attachment = forms.FileField(
+        label=_('Attachments'),
+        required=False,
+    )
 
     agreement = forms.BooleanField(
         label=_('I agree with Code of Conduct'),
         required=True,
     )
+
+    class Meta:
+        model = Proposal
+        fields = ('duration',)
