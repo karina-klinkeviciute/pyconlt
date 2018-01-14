1901cdccee211d5806a38bbe8eb317253582b47e
Šarūnas Navickas <zaibacu@gmail.com>
2018-01-14 18:10:37 +0200

    Protect proposals list

--- proposals/views/manage.py
+++ proposals/views/manage.py
@@ -2,11 +2,13 @@ from django.views.generic import ListView
 from django.views.generic.edit import DeleteView, UpdateView
 from django.urls import reverse_lazy
 from django.utils.decorators import method_decorator
+from django.contrib.auth.decorators import login_required
 
 from ..models.proposal import Proposal
 from ..security import OwnerRequiredMixin
 
 
+@method_decorator(login_required, name='dispatch')
 class ProposalListView(ListView):
     model = Proposal
     template_name = 'proposals/proposal_list.html'
