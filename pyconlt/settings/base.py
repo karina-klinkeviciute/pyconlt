07e0e6031140ce0c3dc9475bede27fd5157a4f0e
karina-klinkeviciute <karina.klinkeviciute@gmail.com>
2018-02-14 19:06:35 +0000

    different redis configuration

--- pyconlt/settings/base.py
+++ pyconlt/settings/base.py
@@ -214,7 +214,7 @@ LOGIN_REDIRECT_URL = 'presenter_update'
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://dev/shm/redis.db",
+        "LOCATION": "dev/shm/redis.db",
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
         }
