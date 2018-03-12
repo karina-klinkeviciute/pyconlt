b8bf42cb65ce09580e3348f383fdb592a0ceed55
karina <karina.klinkeviciute@gmail.com>
2018-03-12 21:48:51 +0000

    fields addes to list_display in slot admin

--- program/admin.py
+++ program/admin.py
@@ -2,5 +2,16 @@ from django.contrib import admin
 
 from program.models import Slot, Track
 
-admin.site.register(Slot)
+
+@admin.register(Slot)
+class SlotAdmin(admin.ModelAdmin):
+    """
+    Admin for slots.
+    """
+    date_hierarchy = 'start_time'
+    list_display = (
+        'name', 'track', 'start_time'
+    )
+
+
 admin.site.register(Track)
