b0fda75b9d6fe291fed1a33b50db0d492dc683a9
karina <karina.klinkeviciute@gmail.com>
2018-04-11 13:41:16 +0100

    started creating models for a separate conference event

---
+++ conference/models/event.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+
+
+class Event(models.Model):
+    """
+    A model for a separate Conference event.
+    """
+    name = models.CharField(
+        max_length=255,
+        help_text=_("A name of the event e.g. PyConLT 2019")
+    )
+    url = models.CharField(
+        help_text=_("The url path for the main page of the conference. "
+                    "e.g. '2018'"),
+    )
+    date = models.DateField(
+        help_text=_("A date of the event or the date of the first day if "
+                    "the event spans more than one day."),
+        blank=True,
+        null=True
+    )
+    duration = models.IntegerField(
+        help_text=_("How many days the event spans"),
+        blank=True,
+        null=True
+    )
