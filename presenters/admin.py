7a4a07b9d0b3e2d86c9b515c3768d1ae81982a7f
karina <karina.klinkeviciute@gmail.com>
2019-01-23 20:42:58 +0000

    added inlint for event to presenter

--- presenters/admin.py
+++ presenters/admin.py
@@ -2,11 +2,19 @@ from django.contrib import admin
 from presenters.models.presenter import Presenter
 
 
+class EventInline(admin.TabularInline):
+    """
+    Inline to show options to choose events.
+    """
+    model = Presenter.event.through
+    raw_id_fields = ("event",)
+
+
 class PresenterAdmin(admin.ModelAdmin):
     list_display = ('name', 'active')
     fieldsets = (
         (None, {
-            'fields': ('user', 'active', 'event'),
+            'fields': ('user', 'active'),
         }),
         ('Personal Info', {
             'fields': ('name', 'image', 'bio', 'expertise')
