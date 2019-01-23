af10da86b293930cd4a8d56c55548186adf068d9
karina <karina.klinkeviciute@gmail.com>
2019-01-23 20:55:51 +0000

    register event in admin

--- presenters/admin.py
+++ presenters/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
+
+from conference.models import Event
 from presenters.models.presenter import Presenter
+
+admin.site.register(Event)
 
 
 class EventInline(admin.TabularInline):
