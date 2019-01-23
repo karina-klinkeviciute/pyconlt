78ed872e04b49a05e083890dfd04c2fdfac4b252
karina <karina.klinkeviciute@gmail.com>
2019-01-23 21:42:36 +0000

    all fields to show

--- presenters/admin.py
+++ presenters/admin.py
@@ -3,27 +3,27 @@ from django.contrib import admin
 from presenters.models.presenter import Presenter
 
 
-class EventInline(admin.TabularInline):
-    """
-    Inline to show options to choose events.
-    """
-    model = Presenter.event.through
+# class EventInline(admin.TabularInline):
+#     """
+#     Inline to show options to choose events.
+#     """
+#     model = Presenter.event.through
 
 
 class PresenterAdmin(admin.ModelAdmin):
     list_display = ('name', 'active')
-    fieldsets = (
-        (None, {
-            'fields': ('user', 'active'),
-        }),
-        ('Personal Info', {
-            'fields': ('name', 'image', 'bio', 'expertise')
-        }),
-        ('Social links', {
-            'fields': ('linkedin_handle', 'twitter_handle', 'github_handle')
-        })
-    )
-    inlines = (EventInline, )
+    # fieldsets = (
+    #     (None, {
+    #         'fields': ('user', 'active'),
+    #     }),
+    #     ('Personal Info', {
+    #         'fields': ('name', 'image', 'bio', 'expertise')
+    #     }),
+    #     ('Social links', {
+    #         'fields': ('linkedin_handle', 'twitter_handle', 'github_handle')
+    #     })
+    # )
+    # inlines = (EventInline, )
 
 
 admin.site.register(Presenter, PresenterAdmin)
