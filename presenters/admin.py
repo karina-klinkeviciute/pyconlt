7aedbd97eecb083552cabc933aef2012d6166f0b
karina <karina.klinkeviciute@gmail.com>
2019-01-23 21:29:26 +0000

    event inline changed

--- presenters/admin.py
+++ presenters/admin.py
@@ -3,30 +3,30 @@ from django.contrib import admin
 from conference.models import Event
 from presenters.models.presenter import Presenter
 
-# admin.site.register(Event)
-#
-#
-# class EventInline(admin.TabularInline):
-#     """
-#     Inline to show options to choose events.
-#     """
-#     model = Presenter.event.through
+admin.site.register(Event)
+
+
+class EventInline(admin.TabularInline):
+    """
+    Inline to show options to choose events.
+    """
+    model = Event
 
 
 class PresenterAdmin(admin.ModelAdmin):
     list_display = ('name', 'active')
-    # fieldsets = (
-    #     (None, {
-    #         'fields': ('user', 'active'),
-    #     }),
-    #     ('Personal Info', {
-    #         'fields': ('name', 'image', 'bio', 'expertise')
-    #     }),
-    #     ('Social links', {
-    #         'fields': ('linkedin_handle', 'twitter_handle', 'github_handle')
-    #     })
-    # )
-    # inlines = (EventInline, )
+    fieldsets = (
+        (None, {
+            'fields': ('user', 'active'),
+        }),
+        ('Personal Info', {
+            'fields': ('name', 'image', 'bio', 'expertise')
+        }),
+        ('Social links', {
+            'fields': ('linkedin_handle', 'twitter_handle', 'github_handle')
+        })
+    )
+    inlines = (EventInline, )
 
 
 admin.site.register(Presenter, PresenterAdmin)
