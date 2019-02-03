0262900ad334aa84d7238bb6849fe612721b2054
karina <karina.klinkeviciute@gmail.com>
2019-02-03 15:09:06 +0000

    removed unused imports

--- proposals/urls.py
+++ proposals/urls.py
@@ -1,6 +1,4 @@
 from django.conf.urls import url
-from django.conf import settings
-from django.conf.urls.static import static
 
 from proposals.views.cfp import CFPView
 from proposals.views.manage import (
